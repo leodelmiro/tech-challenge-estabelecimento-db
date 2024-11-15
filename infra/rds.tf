d5a5d6b5ad508eda84f505d67800869e508f65d4
Leonardo Delmiro <leodelmiroms@gmail.com>
2024-11-15 11:59:14 -0300

    feat: criação rds via terraform

---
+++ infra/rds.tf
@@ -0,0 +1,15 @@
+resource "aws_db_instance" "default" {
+  allocated_storage       = 20
+  db_name                 = var.dbName
+  identifier              = "estabelecimento"
+  engine                  = "postgres"
+  engine_version          = var.engineVersion
+  instance_class          = var.instanceClass
+  username                = var.username
+  password                = var.password
+  backup_retention_period = var.backupRetentionPeriod
+  multi_az                = false
+  vpc_security_group_ids  = [aws_security_group.sg.id]
+  publicly_accessible     = var.publicly_accessible
+  skip_final_snapshot     = var.skipFinalSnapshot
+}
